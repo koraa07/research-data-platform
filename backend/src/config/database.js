5b1572de1dddcb8ea7ee76a27f9b153535d607b1
koraa07 <bjyankarol@gmail.com>
2026-06-09 09:58:23 +0600

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
